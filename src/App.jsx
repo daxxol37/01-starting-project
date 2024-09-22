345601c351a9747c11545ad71e4ac9a2ce4fba47
DASOL\daxxo <daxxolz37@gmail.com>
2024-09-22 17:19:17 +0900

    feat: section2 finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
-
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState(); //초기상태의 것으로 저장시켜야됨
+
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+  console.log("APP COMPONENT EXECUTING");
+
+  let tabContent = <p>Pelase select a topic.</p>
+
+  if (selectedTopic){
+    tabContent = <div id="tab-content">                    
+                    <h3>{EXAMPLES[selectedTopic].title}</h3>
+                    <p>{EXAMPLES[selectedTopic].description}</p>
+                    <pre>
+                      <code>
+                        {EXAMPLES[selectedTopic].code}
+                      </code>
+                    </pre>
+                  </div>
   }
 
   return (
@@ -17,23 +37,33 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem}/>
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          Dynamic Content
+            <TabButton isSelected={selectedTopic === 'components'} 
+            onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'}
+            onSelect={() => handleSelect('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'props'}
+            onSelect={() => handleSelect('props')}>
+              Props
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'state'}
+            onSelect={() => handleSelect('state')}>
+              State
+            </TabButton>
+          </menu>        
+          {tabContent}           
         </section>
-
       </main>
     </div>
   );
